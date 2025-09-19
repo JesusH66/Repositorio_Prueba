f834a0da2e24e6bc0629611140bbbf38660b8750
JesusH66 <jesushernandez6656@gmail.com>
2025-09-19 14:31:25 -0600

    Se realizo la estructura del proyecto con algunos archivos

---
+++ models/Base_Model.php
@@ -0,0 +1,14 @@
+<?php
+class BaseModel {
+    protected $db;
+
+    public function __construct() {
+        // Conexión a la base de datos
+        try {
+            $this->db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Error de conexión a la base de datos: " . $e->getMessage());
+        }
+    }
+}
